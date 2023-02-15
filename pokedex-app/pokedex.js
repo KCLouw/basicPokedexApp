05ced81385e31167577efcd96410dce56432d0be
KCLouw <klouw07@gmail.com>
2023-02-16 00:51:00 +0200

    yeah, this took till morning..

--- pokedex-app/pokedex.js
+++ pokedex-app/pokedex.js
@@ -26,22 +26,40 @@ function logic() {
     /* --- Part 1 : Create pokemon data Object --- */
 
 				// Use If to ensure input validation (ensure input fields are !empty)
-        // Create pokemon object here (const pokemon) using user input Strings
+if(name === "" || type === "" || region === ""){
+    document.getElementById("empty").innerHTML = "Please enter all fields.";
+}else{
+    document.getElementById("empty").innerHTML = ""; 
+}; // Temporay will change once rest is finished
 
+
+        // Create pokemon object here (const pokemon) using user input Strings
+const pokemon = {'name' : name, 'type' : type, 'region' : region};
+console.log(pokemon);
 
         // Create a span element (const pokemonData) and put the pokemon objects properties inside of it
+const pokemonData = document.createElement("span");
+const pokeName = document.createTextNode(pokemon.name);
+pokemonData.appendChild(pokeName);
+const pokeType = document.createTextNode(pokemon.type);
+pokemonData.appendChild(pokeType);
+const pokeRegion = document.createTextNode(pokemon.region);
+pokemonData.appendChild(pokeRegion);
 
+
+console.log(pokemonData);
 
 
     /* --- Part 2 : Creating other DOM Objects --- */
         
         // Create HTML li element here (const pokedexEntry)
-
+const pokedexEntry = document.createElement("li");
 
         // Give pokedexEntry (li) a new class equal to the "type" property of the pokemon object
-
+pokedexEntry.className = pokemon.type;
 
         // Creating a preview button (using a Template Literal)
+        const myBtn = `<button></button>`
         // this code is for an html form that will Google search the name of the pokemon
         const previewButton = `
             <form action="http://google.com/search" target="_blank">
@@ -54,13 +72,20 @@ function logic() {
     /* --- Part 3 : Add things into the DOM --- */
 
         // Firstly append pokemonData to the innerHTML of pokedexEntry
-
+pokedexEntry.innerHTML += `
+<li>${name}</li>
+<li>${type}</li>
+<li>${region}</li>
+`
+//pokedexEntry.innerHTML += pokemonData;
 
         // Secondly append the previewButton to innerHTML of pokedexEntry
-
+pokedexEntry.innerHTML += previewButton;
 
         // Lastly we will get the ul List element that is meant to contain all pokedex entries
+
         // The we will append pokedexEntry to the innerHTML of the pokedexEntries ul element
+document.getElementById("pokedex-entries").appendChild(pokedexEntry);
 
 
 /*-----------------------------------------------
